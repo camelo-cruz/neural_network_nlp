fedfddb252eb210d4cde362e22a57ead29b92e8b
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2023-12-04 10:57:35 +0100

    plots added

--- helper.py
+++ helper.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from model.ffnn import NeuralNetwork, compute_loss
 
 
@@ -27,7 +28,11 @@ def batch_train(X, Y, model, train_flag=False):
     print(f'accuracy before training {accuracy}')
             
     if train_flag:
+        loss_to_print = []
+        acc_to_print = []
+        num_of_iter = 0
         for i in range(1000):
+            num_of_iter += 1
             #Calculating gradient with backpropagation and uploading weights
             dW1, dW2 = model.backward(X, Y)
             model.W1 -= 0.005 * dW1 #for hidden layer
@@ -46,9 +51,12 @@ def batch_train(X, Y, model, train_flag=False):
                              if np.all(preds[:, index] == Y[:, index], axis=0)]
             accuracy = np.sum(equal_columns) / num_columns
             
+            loss_to_print.append(loss)
+            acc_to_print.append(accuracy)
+            
             print(f'epoch {i+1}: model loss {loss}, model accuracy {accuracy}')
             
-    return None
+    return loss_to_print, acc_to_print, num_of_iter
     ###############################################################################
 
 
@@ -59,12 +67,21 @@ def minibatch_train(X, Y, model, train_flag=False):
     #         1) As bonus, train your neural network with batch size = 64
     #         and SGD (batch size = 1) for 1000 epochs using learning rate
     #         = 0.005. Then, plot the cost vs iteration for both cases.
+    
+    batch_model = NeuralNetwork(model.input_size, model.hidden_size, 
+                              model.num_classes, model.seed)
+    loss_batch, acc_batch, num_iter_batch = batch_train(X, Y, batch_model, train_flag=True)
+    
     if train_flag:
+        loss_to_print = []
+        acc_to_print = []
+        num_of_iter = 0
         batch_size = 64
         for i in range(1000):
             #Calculating gradient with backpropagation and uploading weights
             #with minibatch
             for minibatch in create_minibatch(X, Y, batch_size):
+                num_of_iter += 1
                 minibatch_X, minibatch_Y = minibatch
                 dW1, dW2 = model.backward(minibatch_X, minibatch_Y)
                 model.W1 -= 0.005 * dW1 #for hidden layer
@@ -80,7 +97,41 @@ def minibatch_train(X, Y, model, train_flag=False):
             equal_columns = [1 for index in range(num_columns) 
                              if np.all(preds[:, index] == Y[:, index], axis=0)]
             accuracy = np.sum(equal_columns) / num_columns
+            
+            loss_to_print.append(loss)
+            acc_to_print.append(accuracy)
+            
             print(f'epoch {i+1}: model loss {loss}, model accuracy {accuracy}')
+        
+        plt.figure(figsize=(18, 6))
+        
+        plt.subplot(1, 3, 1)
+        plt.bar(['iterations mini_batch', 'iterations batch'], [num_of_iter, num_iter_batch])
+        plt.ylabel('Number of Iterations')
+        plt.title('Number of Iterations')
+        
+
+        plt.subplot(1, 3, 2)
+        plt.plot(loss_to_print, label='Mini-Batch Training')
+        plt.plot(loss_batch, label='Batch Training')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.title('Cost vs Epoch (Mini-Batch Training)')
+        plt.legend()
+        
+
+        plt.subplot(1, 3, 3)
+        plt.plot(acc_to_print, label='Mini-Batch Training')
+        plt.plot(acc_batch, label='Batch Training')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.title('Accuracy vs Epoch (Mini-Batch Training)')
+        plt.legend()
+        
+        plt.tight_layout()
+        plt.savefig('plots/batch_minibatch.png')
+        plt.show()
+        
             
     #########################################################################
 
