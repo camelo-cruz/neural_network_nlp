575ef56c770a7bb0d48acc49b14f0a999c1eea9e
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2023-12-05 17:10:19 +0100

    improved minibatch training

--- helper.py
+++ helper.py
@@ -4,7 +4,7 @@ from model.ffnn import NeuralNetwork, compute_loss
 
 
 
-def batch_train(X, Y, model, train_flag=False):
+def batch_train(X, Y, model, train_flag=False, batch_size=None):
     ################################# STUDENT SOLUTION #############################
     # YOUR CODE HERE
     #     TODO:
@@ -26,18 +26,21 @@ def batch_train(X, Y, model, train_flag=False):
     accuracy = np.sum(equal_columns) / num_columns
     
     print(f'accuracy before training {accuracy}')
+    
+    if not batch_size:
+        batch_size = X.shape[1]
             
     if train_flag:
         loss_to_print = []
         acc_to_print = []
-        num_of_iter = 0
+        iter_batch = 0
         for i in range(1000):
-            num_of_iter += 1
-            #Calculating gradient with backpropagation and uploading weights
-            dW1, dW2 = model.backward(X, Y)
-            model.W1 -= 0.005 * dW1 #for hidden layer
-            model.W_out -= 0.005 * dW2 #for ouput layer
-            
+            for minibatch in create_minibatch(X, Y, batch_size):
+                iter_batch += 1
+                minibatch_X, minibatch_Y = minibatch
+                dW1, dW2 = model.backward(minibatch_X, minibatch_Y)
+                model.W1 -= 0.005 * dW1 #for hidden layer
+                model.W_out -= 0.005 * dW2 #for ouput layer
             
             #Computing metrics for evaluation
             #Computing loss
@@ -55,8 +58,8 @@ def batch_train(X, Y, model, train_flag=False):
             acc_to_print.append(accuracy)
             
             print(f'epoch {i+1}: model loss {loss}, model accuracy {accuracy}')
-            
-    return loss_to_print, acc_to_print, num_of_iter
+    
+    return loss_to_print, acc_to_print, iter_batch
     ###############################################################################
 
 
@@ -68,20 +71,21 @@ def minibatch_train(X, Y, model, train_flag=False):
     #         and SGD (batch size = 1) for 1000 epochs using learning rate
     #         = 0.005. Then, plot the cost vs iteration for both cases.
     
-    batch_model = NeuralNetwork(model.input_size, model.hidden_size, 
+    SGD_model = NeuralNetwork(model.input_size, model.hidden_size, 
                               model.num_classes, model.seed)
-    loss_batch, acc_batch, num_iter_batch = batch_train(X, Y, batch_model, train_flag=True)
+    
+    loss_SGD, acc_SGD, iter_SGD = batch_train(X, Y, SGD_model, 
+                                                    train_flag=True, batch_size=1)
     
     if train_flag:
         loss_to_print = []
         acc_to_print = []
-        num_of_iter = 0
-        batch_size = 64
+        iter_minibatch = 0
         for i in range(1000):
             #Calculating gradient with backpropagation and uploading weights
             #with minibatch
-            for minibatch in create_minibatch(X, Y, batch_size):
-                num_of_iter += 1
+            for minibatch in create_minibatch(X, Y, batch_size=64):
+                iter_minibatch += 1
                 minibatch_X, minibatch_Y = minibatch
                 dW1, dW2 = model.backward(minibatch_X, minibatch_Y)
                 model.W1 -= 0.005 * dW1 #for hidden layer
@@ -106,14 +110,18 @@ def minibatch_train(X, Y, model, train_flag=False):
         plt.figure(figsize=(18, 6))
         
         plt.subplot(1, 3, 1)
-        plt.bar(['iterations mini_batch', 'iterations batch'], [num_of_iter, num_iter_batch])
+        bars = plt.bar(['iterations mini_batch', 'iterations SGD'], [iter_minibatch, iter_SGD])
+        for bar in bars:
+            yval = bar.get_height()
+            plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), ha='center', va='bottom')
+            
         plt.ylabel('Number of Iterations')
         plt.title('Number of Iterations')
         
 
         plt.subplot(1, 3, 2)
         plt.plot(loss_to_print, label='Mini-Batch Training')
-        plt.plot(loss_batch, label='Batch Training')
+        plt.plot(loss_SGD, label='SGD Training')
         plt.xlabel('Epoch')
         plt.ylabel('Loss')
         plt.title('Cost vs Epoch (Mini-Batch Training)')
@@ -122,22 +130,23 @@ def minibatch_train(X, Y, model, train_flag=False):
 
         plt.subplot(1, 3, 3)
         plt.plot(acc_to_print, label='Mini-Batch Training')
-        plt.plot(acc_batch, label='Batch Training')
+        plt.plot(acc_SGD, label='SGD Training')
         plt.xlabel('Epoch')
         plt.ylabel('Accuracy')
         plt.title('Accuracy vs Epoch (Mini-Batch Training)')
         plt.legend()
         
         plt.tight_layout()
-        plt.savefig('plots/batch_minibatch.png')
+        plt.savefig('plots/SGD_minibatch.png')
         plt.show()
+        
         
             
     #########################################################################
 
 def create_minibatch(X, Y, batch_size):
-    indices = np.arange(X.shape[0])
+    indices = np.arange(X.shape[1])
     np.random.shuffle(indices)
-    for i in range(0, X.shape[0] - batch_size + 1, batch_size):
+    for i in range(0, X.shape[1] - batch_size + 1, batch_size):
         batch = indices[i: i + batch_size]
         yield X[:, batch], Y[:, batch]
